390c8061e3c4e8fd72d55ca030b99d9b4d751feb
Ananyaacodes <ananyaaraj21@gmail.com>
2026-05-09 21:18:51 +0530

    Added Q6

---
+++ Assignment1/Q6_isOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q6_isOdd {
+
+    public static boolean isOdd(int n){
+        return ((n & 1 )== 1) ;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.err.println(num+ " is Odd: " +isOdd(num));
+    }
+    
+}
