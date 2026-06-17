5567436055e354962bd1c9af1bfdb38bc7c9c87d
Ananyaacodes <ananyaaraj21@gmail.com>
2026-06-17 23:37:24 +0530

    added q8

---
+++ Assignment3/Q8_Power.java
@@ -0,0 +1,30 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class Q8_Power {
+
+    // Recursive method to calculate x^n
+    public static double power(double x, int n) {
+
+        if (n == 0)
+            return 1;
+
+        return x * power(x, n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of x: ");
+        double x = sc.nextDouble();
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+
+        System.out.println(x + " raised to the power " + n + " = " + power(x, n));
+    }
+}
+    
+
