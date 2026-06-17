f0238be578cb57eecc859809ff63802a734e73da
Ananyaacodes <ananyaaraj21@gmail.com>
2026-06-17 23:46:17 +0530

    added q9 reverse number

---
+++ Assignment3/Q9_ReverseNumber.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+import java.util.Scanner;
+
+public class Q9_ReverseNumber {
+
+    // Recursive method to print digits in reverse
+    public static void reverse(int n) {
+
+        if (n == 0)
+            return;
+
+        System.out.print(n % 10);
+        reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+
+        System.out.print("Reversed Number: ");
+        reverse(num);
+    }
+}
